c06f7c403f702de5defbd1c39c7ca38ab7341a50
Alopex6414 <1029535012@qq.com>
2020-02-16 17:16:35 +0800

    Create logs_simple class.

---
+++ logs/logs_simple.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 10295 on 2020/2/16.
+//
+
+#include "logs_simple.h"
---
+++ logs/logs_simple.h
@@ -0,0 +1,14 @@
+//
+// Created by 10295 on 2020/2/16.
+//
+
+#ifndef TWILIGHT_LOGS_SIMPLE_H
+#define TWILIGHT_LOGS_SIMPLE_H
+
+
+class logs_simple {
+
+};
+
+
+#endif //TWILIGHT_LOGS_SIMPLE_H
