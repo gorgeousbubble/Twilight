5090f293487aac751a0015baac701e1bb6181745
Alopex6414 <1029535012@qq.com>
2020-02-23 20:50:34 +0800

    [feature]Create dev-utils.

---
+++ utils/utils_threadsafe.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 10295 on 2020/2/23.
+//
+
+#include "utils_threadsafe.h"
---
+++ utils/utils_threadsafe.h
@@ -0,0 +1,14 @@
+//
+// Created by 10295 on 2020/2/23.
+//
+
+#ifndef TWILIGHT_UTILS_THREADSAFE_H
+#define TWILIGHT_UTILS_THREADSAFE_H
+
+
+class utils_threadsafe {
+
+};
+
+
+#endif //TWILIGHT_UTILS_THREADSAFE_H
