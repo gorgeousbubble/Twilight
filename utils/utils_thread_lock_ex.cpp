bfe2f7a8d17430adc74c112f4ce56d2255f5a02f
Alopex6414 <1029535012@qq.com>
2020-02-24 15:11:01 +0800

    [improve]Append comments

--- utils/utils_thread_lock_ex.cpp
+++ utils/utils_thread_lock_ex.cpp
@@ -13,22 +13,57 @@
 
 #include "utils_thread_lock_ex.h"
 
+//----------------------------------------------
+// @Function: utils_thread_lock_ex
+// @Purpose: construct function
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//----------------------------------------------
 utils_thread_lock_ex::utils_thread_lock_ex() {
     ::InitializeCriticalSection(&m_csLock);
 }
 
+//----------------------------------------------
+// @Function: ~utils_thread_lock_ex
+// @Purpose: destruct function
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//----------------------------------------------
 utils_thread_lock_ex::~utils_thread_lock_ex() {
     ::DeleteCriticalSection(&m_csLock);
 }
 
+//----------------------------------------------
+// @Function: get
+// @Purpose: get the critical section pointer
+// @Since: v1.00a
+// @Para: None
+// @Return: m_csLock's pointer
+//----------------------------------------------
 CRITICAL_SECTION *utils_thread_lock_ex::get() {
     return &m_csLock;
 }
 
+//----------------------------------------------
+// @Function: enter
+// @Purpose: enter critical section
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//----------------------------------------------
 void utils_thread_lock_ex::enter() {
     ::EnterCriticalSection(&m_csLock);
 }
 
+//----------------------------------------------
+// @Function: leave
+// @Purpose: leave critical section
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//----------------------------------------------
 void utils_thread_lock_ex::leave() {
     ::LeaveCriticalSection(&m_csLock);
 }
