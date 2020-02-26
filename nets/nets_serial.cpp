a46ed1c0cfe1fbadc8c40694d897b8e5f60c9790
Alopex6414 <1029535012@qq.com>
2020-02-26 13:54:44 +0800

    [feature]Migrate part code from Rosa

--- nets/nets_serial.cpp
+++ nets/nets_serial.cpp
@@ -12,3 +12,67 @@
 */
 
 #include "nets_serial.h"
+
+//----------------------------------------------
+// @Function: nets_serial
+// @Purpose: construct function
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//----------------------------------------------
+nets_serial::nets_serial() {
+    // initial value
+    m_hCom = INVALID_HANDLE_VALUE;
+    m_hListen = INVALID_HANDLE_VALUE;
+    m_bOpen = false;
+    m_bRecv = false;
+    // clean count
+    m_dwSendCount = 0;
+    m_dwRecvCount = 0;
+    // clean memory
+    memset(&m_ovWrite, 0, sizeof(m_ovWrite));
+    memset(&m_ovRead, 0, sizeof(m_ovRead));
+    memset(&m_ovWait, 0, sizeof(m_ovWait));
+    memset(m_chSendBuf, 0, sizeof(m_chSendBuf));
+    memset(m_chRecvBuf, 0, sizeof(m_chRecvBuf));
+    // initial critical section
+    ::InitializeCriticalSection(&m_csComSync);
+}
+
+//----------------------------------------------
+// @Function: ~nets_serial
+// @Purpose: destruct function
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//----------------------------------------------
+nets_serial::~nets_serial() {
+    // close open flag
+    ::EnterCriticalSection(&m_csComSync);
+    m_bOpen = false;
+    ::LeaveCriticalSection(&m_csComSync);
+    // clean event flags
+    if (m_hCom != INVALID_HANDLE_VALUE) {
+        ::CloseHandle(m_hCom);
+        m_hCom = INVALID_HANDLE_VALUE;
+    }
+    if (m_ovWrite.hEvent != NULL) {
+        ::CloseHandle(m_ovWrite.hEvent);
+        m_ovWrite.hEvent = NULL;
+    }
+    if (m_ovRead.hEvent != NULL) {
+        ::CloseHandle(m_ovRead.hEvent);
+        m_ovRead.hEvent = NULL;
+    }
+    if (m_ovWait.hEvent != NULL) {
+        ::CloseHandle(m_ovWait.hEvent);
+        m_ovWait.hEvent = NULL;
+    }
+    if (m_hListen != INVALID_HANDLE_VALUE) {
+        ::WaitForSingleObject(m_hListen, INFINITE);
+        ::CloseHandle(m_hListen);
+        m_hListen = INVALID_HANDLE_VALUE;
+    }
+    // delete critical section
+    ::DeleteCriticalSection(&m_csComSync);
+}
--- nets/nets_serial.h
+++ nets/nets_serial.h
@@ -14,8 +14,47 @@
 #ifndef TWILIGHT_NETS_SERIAL_H
 #define TWILIGHT_NETS_SERIAL_H
 
+#include <Windows.h>
 
-class nets_serial {
+#include <iostream>
+#include <map>
+#include <vector>
+#include <process.h>
+
+#pragma comment(lib, "WinMM.lib")
+
+#ifdef TWILIGHT_EXPORTS
+#define TWILIGHT_NETS_SERIAL_API __declspec(dllexport)
+#else
+#define TWILIGHT_NETS_SERIAL_API __declspec(dllimport)
+#endif
+
+#define TWILIGHT_NETS_SERIAL_CALLMETHOD __stdcall
+
+#define NETS_SERIAL_BUFFER_SIZE 4096
+
+class TWILIGHT_NETS_SERIAL_API nets_serial {
+private:
+    HANDLE m_hCom;
+    HANDLE m_hListen;
+
+    OVERLAPPED m_ovWrite;
+    OVERLAPPED m_ovRead;
+    OVERLAPPED m_ovWait;
+
+public:
+    CRITICAL_SECTION m_csComSync;
+    volatile bool m_bOpen;
+    volatile bool m_bRecv;
+    std::map<int, std::string> m_mapCom;
+
+    UCHAR m_chSendBuf[NETS_SERIAL_BUFFER_SIZE];
+    UCHAR m_chRecvBuf[NETS_SERIAL_BUFFER_SIZE];
+    DWORD m_dwSendCount;
+    DWORD m_dwRecvCount;
+
+    nets_serial();
+    ~nets_serial();
 
 };
 
