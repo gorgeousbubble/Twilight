a400bf534ee401ed1b80a7ca9a4299a45f249400
Alopex6414 <1029535012@qq.com>
2020-02-26 13:07:00 +0800

    [feature]Create nets_serial

---
+++ nets/nets_serial.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 10295 on 2020/2/26.
+//
+
+#include "nets_serial.h"
---
+++ nets/nets_serial.h
@@ -0,0 +1,14 @@
+//
+// Created by 10295 on 2020/2/26.
+//
+
+#ifndef TWILIGHT_NETS_SERIAL_H
+#define TWILIGHT_NETS_SERIAL_H
+
+
+class nets_serial {
+
+};
+
+
+#endif //TWILIGHT_NETS_SERIAL_H
