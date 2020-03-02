3b8a6eee232a0462c212a95814bfc5d9958af888
Alopex6414 <1029535012@qq.com>
2020-03-02 10:45:24 +0800

    Create nets_socket class

---
+++ nets/nets_socket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 10295 on 2020/3/2.
+//
+
+#include "nets_socket.h"
---
+++ nets/nets_socket.h
@@ -0,0 +1,14 @@
+//
+// Created by 10295 on 2020/3/2.
+//
+
+#ifndef TWILIGHT_NETS_SOCKET_H
+#define TWILIGHT_NETS_SOCKET_H
+
+
+class nets_socket {
+
+};
+
+
+#endif //TWILIGHT_NETS_SOCKET_H
