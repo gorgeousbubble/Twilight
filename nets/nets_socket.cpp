0b7d7fa3328665d75ae1db78f2acd330b0c020d1
Alopex6414 <1029535012@qq.com>
2020-03-02 10:49:27 +0800

    [improve]Append comments on top

--- nets/nets_socket.cpp
+++ nets/nets_socket.cpp
@@ -1,5 +1,14 @@
-//
-// Created by 10295 on 2020/3/2.
-//
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017~2020, Gorgeous Bubble
+*     All rights reserved.
+*
+* @proj     Twilight
+* @file     nets_socket.cpp
+* @brief    The Twilight Project.
+* @author   alopex
+* @version  v1.00a
+* @date     2020-03-02
+*/
 
 #include "nets_socket.h"
--- nets/nets_socket.h
+++ nets/nets_socket.h
@@ -1,6 +1,15 @@
-//
-// Created by 10295 on 2020/3/2.
-//
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017~2020, Gorgeous Bubble
+*     All rights reserved.
+*
+* @proj     Twilight
+* @file     nets_socket.h
+* @brief    The Twilight Project.
+* @author   alopex
+* @version  v1.00a
+* @date     2020-03-02
+*/
 
 #ifndef TWILIGHT_NETS_SOCKET_H
 #define TWILIGHT_NETS_SOCKET_H
